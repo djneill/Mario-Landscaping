665c5a99c2f03a0838d90db5b3bced62483916f2
DJ Neill <dj.neill856@gmail.com>
2024-04-01 23:31:52 -0500

    mobile overflow

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,9 @@ import { AiFillPhone } from 'react-icons/ai'
 const Navbar = () => {
     return (
         <>
-            <div className="navbar bg-green-200 fixed z-[2] overflow-hidden">
+            <div className="navbar bg-green-200  z-[2] ">
                 <div className="navbar-start">
-                    <div className="dropdown">
+                    <div className="dropdown lg:hidden">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
