2fb00198338afb737fe15982b385950402804f00
DJ Neill <dj.neill856@gmail.com>
2024-05-04 20:50:07 -0500

    form action

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -16,7 +16,7 @@ const Contact = () => {
             <div className="p-4 mx-auto">
                 <h2 className="text-2xl font-bold text-center text-shadow">Contact Us</h2>
                 <p className="text-base font-bold mx-4 text-white">Call us today <a className='text-blue-600' href='tel:2544216398'>254-421-6398</a> or send a message below.</p>
-                <form className="mt-4" name="contact" method="POST" data-netlify="true">
+                <form className="mt-4" name="contact" method="POST" data-netlify="true" action="/netlify-form">
                     <input type="hidden" name="form-name" value="contact" />
                     <div className="mb-4 mx-4">
                         <label className="block text-gray-600" htmlFor="email">Email:</label>
